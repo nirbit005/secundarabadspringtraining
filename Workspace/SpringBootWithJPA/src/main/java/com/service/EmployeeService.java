ab74ea9d9e4c7e360dedf15b4159ecbf13e5f553
nirbit005 <49370954+nirbit005@users.noreply.github.com>
2019-09-08 01:24:10 +0530

    Add files via upload
    Working code of transaction rollback. - Earlier saved data will be rollbacked if later one failed.

--- Workspace/SpringBootWithJPA/src/main/java/com/service/EmployeeService.java
+++ Workspace/SpringBootWithJPA/src/main/java/com/service/EmployeeService.java
@@ -3,73 +3,106 @@ package com.service;
 import java.util.List;
 import java.util.ListIterator;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.Isolation;
+
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.bean.Employee;
 import com.bean.TrainerStudent;
-import com.main.EmployeeDao;
-@Service
+import com.dao.EmployeeDao;
 
+@Service
 public class EmployeeService {
+
 	@Autowired
 	EmployeeDao employeeDao;
 	
-	public List<Employee> getEmployeeInfo() {
+	// Below code is without JPA
+/*	
+	  public Employee getEmployeeInfo(int id) {
+		return employeeDao.getEmployeeInfo(id);
+	}
+	  
+	  public String storeEmployeeInfo(Employee emp) {
+		  
+		  if(employeeDao.storeEmployeeInfo(emp) > 0) {
+			  return "Record Strored Successfully";
+		  }else {
+			  return "Record Not Strored"; 
+		  }
+		  
+	  }
+	  
+	  public String updateEmployeeInfo(Employee emp) {
+		  
+		  if(employeeDao.updateEmployeeInfo(emp) > 0) {
+			  return "Record Updated Successfully";
+		  }else {
+			  return "Record Not Updated"; 
+		  }
+		  
+	  }
+	  
+	  public String deleteEmployeeInfo(int id) {
+		  
+		  if(employeeDao.deleteEmployeeInfo(id) > 0) {
+			  return "Record Deleted Successfully";
+		  }else {
+			  return "Record Not Deleted"; 
+		  }
+		  
+	  }
+	  
+	  */
+	
+	// Below part is with JPA
+	
+	public List<Employee> getEmployeeInfo(){
+		
 		List<Employee> listOfRec = employeeDao.getEmployeeInfo();
 		ListIterator<Employee> ll = listOfRec.listIterator();
 		while(ll.hasNext()) {
 			Employee emp = ll.next();
 			emp.setSalary(emp.getSalary()+10000);
 		}
+		
 		return listOfRec;
+		
 	}
 	
-	public List<TrainerStudent> getTrainerStudent() {
+	public List<TrainerStudent> getTrainerStudent(){
 		return employeeDao.getTrainerStudentInfo();
 	}
 	
+	//If Transactional annotation added in service layer then 
+	//   comment the begin and commit part on DAO layer which was used for maintaining transaction.
+//	@Transactional()   ---->AT Present not working so commented and begin and comiit restored.
+	
+	
 	@Transactional(propagation = Propagation.REQUIRED,
-			rollbackFor = Exception.class,timeout = 5)
+			rollbackFor  = Exception.class, timeout = 5)
+//	@Transactional
 	public String storeEmployeeInfo(Employee emp) {
 		try {
 			Thread.sleep(2000);
-			employeeDao.deleteStudentInfoById(emp.getId());
-			return employeeDao.storeEmployeeInfo(emp);	
-		}catch(Exception e) {}
-		return null;
-	}
-	
-	/*
-	public Employee getEmployeeInfo(int id) {
-			return employeeDao.getEmployeeInfo(id);
-	}
-	
-	public String storeEmployeeInfo(Employee emp) {
-				if(employeeDao.storeEmployeeInfo(emp)>0) {
-					return "Record stored successfully";
-				}else {
-					return "Record not store";
-				}
-	}
-	
-	public String updateEmployeeInfo(Employee emp) {
-		if(employeeDao.updateEmployeeInfo(emp)>0) {
-			return "Record updated successfully";
-		}else {
-			return "Record not preset";
+			employeeDao.storeEmployeeInfo(emp);	
+			String msg="";
+			System.out.println("Emp Id is - "+ emp.getId());
+			if(employeeDao.deleteStudentInfoById(emp.getId()) > 0)
+				msg="Deletion Success";
+			else {
+				msg="Deletion failed";
+				throw new java.lang.Exception();
+			}
+			System.out.println(msg);
+			return msg;
+		//	return employeeDao.storeEmployeeInfo(emp);
+		}catch(Exception e) {
+			TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
 		}
+		return "Data Store Service Failed";
 	}
-	public String deleteEmployeeInfo(int id) {
-		if(employeeDao.deleteEmployeeInfo(id) >0) {
-			return "Record deleted successfully";
-		}else {
-			return "Record not preset";
-		}
-	}*/
 }
-
